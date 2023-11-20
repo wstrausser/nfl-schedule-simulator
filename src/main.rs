58bae10e5569c27301aac114dd3dcd976fab92bf
William E. Strausser <william.e.strausser@gmail.com>
2023-11-20 23:48:01 +0000

    Implemented timing on main function to test performance of simulation algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use nfl_schedule_simulator::*;
+use std::io::Write;
+use std::time::Instant;
 
 fn main() {
     let season_year: i32 = 2023;
     let mut season: Season = Season::new_from_year(season_year);
 
-    season.run_simulation();
-
-    println!("{:#?}", season.simulated_games);
+    let now: Instant = Instant::now();
+    for i in 0..10000 {
+        season.run_simulation();
+        print!("\r{i}");
+        std::io::stdout()
+            .flush()
+            .expect("stdout could not be flushed");
+    }
+    let elapsed: std::time::Duration = now.elapsed();
+    println!("\n{:.2?}", elapsed);
 }
