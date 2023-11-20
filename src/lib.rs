86ebae58c8bfe31b2fd6d3f6b1ba7f18d0f20b48
William E. Strausser <william.e.strausser@gmail.com>
2023-11-20 06:49:17 +0000

    Implemented fetching of games

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use postgres::{Client, Error, NoTls};
+use postgres::{Client, Error, NoTls, Row};
 use std::collections::HashMap;
 use std::env::var;
 
@@ -21,18 +21,12 @@ pub struct Team {
 
 #[derive(Debug)]
 pub struct Game<'a> {
-    pub game_id: u32,
-    pub season: Season<'a>,
-    pub week: u32,
+    pub game_id: i32,
+    pub season: i32,
+    pub week: i32,
     pub home_team: &'a Team,
     pub away_team: &'a Team,
     pub game_result: Option<GameResult>,
-}
-
-#[derive(Debug)]
-pub struct Season<'a> {
-    pub year: u32,
-    pub games: Vec<Game<'a>>,
 }
 
 pub fn get_season_teams(season: i32) -> HashMap<i32, Team> {
@@ -54,15 +48,7 @@ pub fn get_season_teams(season: i32) -> HashMap<i32, Team> {
     "
     );
 
-    let mut client: Client = match connect() {
-        Ok(c) => c,
-        Err(e) => panic!("{}", e),
-    };
-
-    let results = match client.query(&query, &[]) {
-        Ok(r) => r,
-        Err(e) => panic!("{}", e),
-    };
+    let results: Vec<Row> = run_query(query);
 
     let mut teams: HashMap<i32, Team> = HashMap::new();
     for row in results {
@@ -76,6 +62,63 @@ pub fn get_season_teams(season: i32) -> HashMap<i32, Team> {
         teams.insert(team.team_id, team);
     }
     teams
+}
+
+pub fn get_season_games<'a>(season: i32, teams: &'a HashMap<i32, Team>) -> HashMap<i32, Game<'a>> {
+    let query: String = format!(
+        "
+        SELECT
+            game_id,
+            week,
+            home_team_id,
+            away_team_id,
+            home_score,
+            away_score
+        FROM nfl.games
+        WHERE season={season};
+    "
+    );
+
+    let results: Vec<Row> = run_query(query);
+
+    let mut games: HashMap<i32, Game> = HashMap::new();
+    for row in results {
+        let game_id: i32 = row.get(0);
+        let week: i32 = row.get(1);
+        let home_team_id: i32 = row.get(2);
+        let away_team_id: i32 = row.get(3);
+        let home_score: Option<i32> = row.get(4);
+        let away_score: Option<i32> = row.get(5);
+
+        let home_team: &Team = teams.get(&home_team_id).expect("Team does not exist");
+        let away_team: &Team = teams.get(&away_team_id).expect("Team does not exist");
+
+        let game_result: Option<GameResult> = {
+            if home_score.is_none() && away_score.is_none() {
+                None
+            }
+            else if home_score.unwrap() > away_score.unwrap() {
+                Some(GameResult::HomeWin)
+            }
+            else if home_score.unwrap() < away_score.unwrap() {
+                Some(GameResult::AwayWin)
+            }
+            else {
+                Some(GameResult::Tie)
+            }
+        };
+
+        let game: Game = Game {
+            game_id: game_id,
+            season: season,
+            week: week,
+            home_team: home_team,
+            away_team: away_team,
+            game_result: game_result,
+        };
+        games.insert(game_id, game);
+    }
+    games
 }
 
 fn get_variable(key: &str) -> String {
@@ -96,7 +139,20 @@ fn get_conn_string() -> String {
     format!("postgres://{pg_user}:{pg_pass}@{pg_locn}/{pg_dtbs}")
 }
 
-fn connect() -> Result<Client, Error> {
+fn connect() -> Client {
     let conn_string = get_conn_string();
-    Client::connect(&conn_string, NoTls)
+    let mut client: Client = match Client::connect(&conn_string, NoTls) {
+        Ok(c) => c,
+        Err(e) => panic!("{}", e),
+    };
+    client
+}
+
+fn run_query(query: String) -> Vec<Row> {
+    let mut client: Client = connect();
+    let results = match client.query(&query, &[]) {
+        Ok(r) => r,
+        Err(e) => panic!("{}", e),
+    };
+    results
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use nfl_schedule_simulator::*;
+use std::collections::HashMap;
 
 fn main() {
+    let season: i32 = 2023;
     let teams = get_season_teams(2023);
+    let games: HashMap<i32, Game> = get_season_games(season, &teams);
 
-    println!("{:?}", teams);
+    let game: &Game = games.get(&3924).unwrap();
+
+    let division_game: bool = {
+        game.home_team.division == game.away_team.division
+    };
+
+    println!("{:?}", division_game);
 }
